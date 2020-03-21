7f0acd61f2c6160fd25ded21ca51d24a3cb54b68
Damian Osiecki <osieckidamian1992@gmail.com>
2020-03-21 18:25:36 +0100

    Add array object of todo items

---
+++ src/components/TodoList.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <input type="text" class="todo-input" placeholder="What needs to be done..." v-model="newTodo">
+    <div v-for="todo in todos" :key="todo.id" class="todo-item">
+      {{ todo.title}}
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'todo-list',
+    data() {
+      return {
+        newTodo: '',
+        todos: [
+          {
+            'id': 1,
+            'title': 'Finish Vue Screencast ',
+            'completed': false,
+          },
+          {
+            'id': 2,
+            'title': 'Take over world',
+            'completed': false,
+          },
+          {
+            'id': 3,
+            'title': 'Elo ',
+            'completed': false,
+          },          
+        ]
+      }
+    }
+  }
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+  .todo-input {
+    width: 100%;
+    padding: 10px 18px;
+    font-size: 18px;
+    margin-bottom: 16px;
+
+    &:focus {
+      outline: 0;
+    }
+  }
+
+</style>
